fbcc6dea6a1e405e6f0c572ff2f19fde7e997c14
Nathan Lauder <56481793+nathanlauder@users.noreply.github.com>
2022-01-11 13:31:15 -0600

    Delete git.sh

--- git.sh
+++
@@ -1,6 +0,0 @@
-# brew install git
-
-git config --global user.name "Nathan Lauder"
-git config --global user.email "laudern42@gmail.com"
-git config --global core.editor vim
-git config --global color.ui auto
